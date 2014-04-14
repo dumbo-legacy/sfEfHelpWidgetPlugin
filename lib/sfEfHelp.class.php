9ff843550d274c060e5396a0f2828821f79975fa
ympons <yaismelmp@googlemail.com>
2014-04-14 15:00:56 -0400

    Added library

---
+++ lib/sfEfHelp.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @desc  Plugin to load help widget. 
+ * yaismelmp@googlemail.com
+ *
+ */
+class sfEfHelp
+{
+
+  public static function load()
+  {
+	  if ( isset( $_SERVER ) ) {
+		  $sAgent = $_SERVER['HTTP_USER_AGENT'] ;
+	  }
+	  else {
+		  global $HTTP_SERVER_VARS ;
+		  if ( isset( $HTTP_SERVER_VARS ) ) {
+			  $sAgent = $HTTP_SERVER_VARS['HTTP_USER_AGENT'] ;
+		  }
+		  else {
+			  global $HTTP_USER_AGENT ;
+			  $sAgent = $HTTP_USER_AGENT ;
+		  }
+	  }
+	
+    $help_path = sfConfig::get('sfhelp_web_dir', '/sfEfHelpWidgetPlugin');
+    
+    /* Load jQuery core */
+    $filename = $help_path.'/js/'.sfConfig::get('jquery_core', 'jquery-1.4.3.min.js');
+    sfContext::getInstance()->getResponse()->addJavascript($filename);
+    
+    /* To fix z-index problems in IE6, include the bgiframe plugin on your page */
+    $filename = $help_path.'/js/'.sfConfig::get('jquery_bgiframe', 'jquery.bgiframe.min.js');
+    sfContext::getInstance()->getResponse()->addJavascript($filename);
+    
+    /* [if IE] load excanvas.js */
+    if ( strpos($sAgent, 'MSIE') !== false )
+    {
+      $filename = $help_path.'/js/'.sfConfig::get('canvas', 'excanvas.js');
+      sfContext::getInstance()->getResponse()->addJavascript($filename);
+    }
+      
+    /* Load BeautyTips */
+    $filename = $help_path.'/js/'.sfConfig::get('jquery_bt', 'jquery.bt.min.js');
+    sfContext::getInstance()->getResponse()->addJavascript($filename);
+    
+    /* Load principal functions */
+    $filename = $help_path.'/js/'.sfConfig::get('efhelpwidget', 'helpwidget.js');
+    sfContext::getInstance()->getResponse()->addJavascript($filename);  
+  }
+
+}  
